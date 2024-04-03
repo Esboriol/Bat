d5085d7494996cb07cb7a65ac57b2849a6c714c5
Esboriol <otavioesboriol@gmail.com>
2024-04-03 16:00:22 -0300

    Execultavel.bat

---
+++ abre_vscode.bat
@@ -0,0 +1,4 @@
+# Script para abrir o visual studio code em uma pasta especifica tambem vai abrir o navegador com o arquivo index.html
+
+start chrome C:\Users/2_DEV/aulas/html/pag1.html
+code C:\Users\2_DEV\aulas
